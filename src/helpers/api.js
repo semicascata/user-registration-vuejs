72783cdae8a659636f1d1d9c1f66dde161de1d86
r. drt <51922521+semicascata@users.noreply.github.com>
2021-02-20 18:01:15 -0300

    change axios url calls for production

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3003/api/v1/usreg/",
+  // baseURL: "http://localhost:3003/api/v1/usreg/", // dev-local url
+  baseURL: "https://user-registration-nestjs.herokuapp.com/api/v1/usreg/", // heroku server
   headers: {
     "Content-Type": "application/json",
   },
